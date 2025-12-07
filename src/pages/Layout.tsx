e9c3b85726a249483379c11205bb4b91bf9b05e9
nuthdev <nuthmc7@gmail.com>
2025-12-07 09:56:45 +0700

    development

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,8 +4,9 @@ import {Outlet} from "react-router-dom";
 const Layout = () => {
     return (
         <>
-            {/* <Navbar /> */}
+            {/* <Navbar /> */}  
             <Outlet/>
+            
             {/* <Footer/> */}
         </>
     )
